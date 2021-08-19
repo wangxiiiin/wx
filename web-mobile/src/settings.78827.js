eafdf02057a620b86f99705bdb07758627cd6569
wangxiiiin <89178987+wangxiiiin@users.noreply.github.com>
2021-08-19 14:11:20 +0800

    Add files via upload

---
+++ web-mobile/src/settings.78827.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/runGame.fire",orientation:"",jsList:[],bundleVers:{internal:"b25de",main:"e6d61"}};
